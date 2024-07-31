41abbc961f2b31f43b4f9aa8524fb250d56050c2
aiswarya-das <aiswaryadas.a@gmail.com>
2024-07-31 23:53:04 +0530

    all files modified

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,34 +1,34 @@
-import { DraggableLocation } from "react-beautiful-dnd";
+// import { DraggableLocation } from "react-beautiful-dnd";
 
-export type Task = {
-  id: string;
-  task: string;
-  tag: string;
-  date: string;
-  description: string;
-};
+// export type Task = {
+//   id: string;
+//   task: string;
+//   tag: string;
+//   date: string;
+//   description: string;
+// };
 
-export type TaskMap = {
-  [key: string]: Task[];
-};
+// export type TaskMap = {
+//   [key: string]: Task[];
+// };
 
-export type Board = {
-  columns: TaskMap;
-  ordered: string[];
-};
+// export type Board = {
+//   columns: TaskMap;
+//   ordered: string[];
+// };
 
-export type BoardAction =
-  | { type: "SET_TASKS"; payload: Board }
-  | { type: "ADD_TASK"; payload: Task }
-  | { type: "REMOVE_TASK"; payload: RemoveTaskPayload }
-  | { type: "MOVE_TASK"; payload: OnDragPayload }
-  | { type: "MOVE_COLUMN"; payload: OnDragPayload };
+// export type BoardAction =
+//   | { type: "SET_TASKS"; payload: Board }
+//   | { type: "ADD_TASK"; payload: Task }
+//   | { type: "REMOVE_TASK"; payload: RemoveTaskPayload }
+//   | { type: "MOVE_TASK"; payload: OnDragPayload }
+//   | { type: "MOVE_COLUMN"; payload: OnDragPayload };
 
-type OnDragPayload = {
-  source: DraggableLocation;
-  destination: DraggableLocation;
-};
+// type OnDragPayload = {
+//   source: DraggableLocation;
+//   destination: DraggableLocation;
+// };
 
-type RemoveTaskPayload = {
-  id: string;
-};
+// type RemoveTaskPayload = {
+//   id: string;
+// };
